62b0de2d986a4c856c43518ef787bcb4dc768e8c
Raghav Sharma <code.practice17@gmail.com>
2025-05-20 20:23:54 +0530

    Basic semi colon parsing

--- src/parsing.c
+++ src/parsing.c
@@ -4,8 +4,26 @@
 
 
 void
+parse_semicolons(char **tokens)
+{
+    /* check for semi-colons and replace them with NULL */
+    size_t i = 0;
+
+    while(tokens[i] != NULL) {
+        if(!strcmp(tokens[i], ";")) {
+            tokens[i] = NULL;
+        }
+
+        i++;
+    }
+}
+
+
+void
 parse_and_execute(char **tokens)
 {
+    parse_semicolons(tokens);
+
     if (is_builtin(tokens[0])) {
         handle_builtin(tokens);
     } else {
