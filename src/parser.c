b122020cdb564177979bb90c6e433328c1ba65e6
Raghav Sharma <code.practice17@gmail.com>
2025-06-20 20:51:11 +0530

    fixed sequence parser

--- src/parser.c
+++ src/parser.c
@@ -3,19 +3,36 @@
 Element *
 parse_condition(Element *elements, size_t *pos)
 {
+    Element *left = &elements[*pos];
+    (*pos)++;
 
+    while(elements[*pos].element_type != NIL &&
+          elements[*pos].element_type != NEXT) {
+
+        elements[*pos].left = left;
+        left = &elements[*pos];
+        (*pos)++;
+        left->right = &elements[*pos];
+        (*pos)++;
+    }
+
+    Element *root = left;
+
+    return root;
 }
 
 Element *
-parse_sequence(Element *elements, size_t pos)
+parse_sequence(Element *elements)
 {
+    size_t pos = 0;
+
     Element *left = parse_condition(elements, &pos);
 
     while(elements[pos].element_type != NIL) {
         elements[pos].left = left;
         left = &elements[pos];
         pos++;
-        elements[pos].right = parse_condition(elements, &pos);
+        left->right = parse_condition(elements, &pos);
     }
 
     Element *ast_root = left;
@@ -26,6 +43,5 @@ parse_sequence(Element *elements, size_t pos)
 void
 parse_and_evaluate (Element *elements)
 {
-    size_t pos = 0;
-    Element *ast_root = parse_sequence(elements, pos);     
+    Element *ast_root = parse_sequence(elements);     
 }
