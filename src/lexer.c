98a4d1804b75599ad50125284167e54a9c2df869
Raghav Sharma <code.practice17@gmail.com>
2025-07-15 10:17:13 +0530

    lexer: recreated `tokenize_command`; getting another bug

--- src/lexer.c
+++ src/lexer.c
@@ -6,55 +6,16 @@
 
 /* `free`s memory allocated to tokens in case of error */
 void
-clean_memory(Token **tokens, ssize_t token_index)
+clean_memory (Token **tokens, ssize_t token_index)
 {
   for (ssize_t i = 0; i < token_index; i++)
     {
       if ((*tokens)[i].type == COMMAND)
         for (size_t i = 1; i <= (*tokens)->argc; i++)
-          free((*tokens)->argv[i - 1]);
+          free ((*tokens)->argv[i - 1]);
     }
 
-  free(tokens);
-}
-
-Token
-init_token(Token_type token_type)
-{
-  Token token;
-
-  token.type          = token_type;
-  token.argv          = NULL;
-  token.argc          = 0;
-  token.left          = NULL;
-  token.right         = NULL;
-  token.return_status = NOT_DEFINED;
-
-  return token;
-}
-
-/*
- * Adds token in `tokens` array
- */
-int
-add_token(Token **tokens, ssize_t *token_index, Token_type token_type)
-{
-  /* `*token_index` starts from `-1` */
-  *token_index = *token_index + 1;
-
-  *tokens = realloc (*tokens, (*token_index + 1) * sizeof (**tokens));
-
-  /* `realloc` fails */
-  if (*tokens == NULL)
-    {
-      perror("realloc failed in `add_token`");
-      return -1;
-    }
-
-  /* Initialize the token with default values */
-  (*tokens)[*token_index] = init_token(token_type);
-
-  return 0;
+  free (tokens);
 }
 
 /*
@@ -63,7 +24,7 @@ add_token(Token **tokens, ssize_t *token_index, Token_type token_type)
  * of `type` `COMMAND`
  */
 int
-reallocate_arg_array(Token *token)
+reallocate_arg_array (Token *token)
 {
   char **argv = token->argv;
   size_t argc = token->argc;
@@ -71,39 +32,39 @@ reallocate_arg_array(Token *token)
   /* `argc` starts from `0` */
   argc++;
 
-  argv = realloc(argv, argc * sizeof (*argv));
+  argv = realloc (argv, argc * sizeof (*argv));
 
   if (argv == NULL)
     {
-      perror("reallocate_arg_array");
+      perror ("reallocate_arg_array");
       return -1;
     }
 
   /* Initialize with `NULL` */
   argv[argc - 1] = NULL;
 
-  token->argv    = argv;
-  token->argc    = argc;
+  token->argv = argv;
+  token->argc = argc;
 
   return 0;
 }
 
 /*
  * For each argument in `argv` array of `COMMAND`
- * allocate memory for argument string 
+ * allocate memory for argument string
  */
 int
-allocate_mem_for_string(size_t upper_lim, Token *token)
+allocate_mem_for_string (size_t upper_lim, Token *token)
 {
   char **argv = token->argv;
   size_t argc = token->argc;
 
   /* `upper_lim + 1`: `1` extra byte for null-character */
-  argv[argc - 1] = malloc(upper_lim + 1);
+  argv[argc - 1] = malloc (upper_lim + 1);
 
   if (argv[argc - 1] == NULL)
     {
-      perror("allocate_mem_for_string");
+      perror ("allocate_mem_for_string");
       return -1;
     }
 
@@ -115,9 +76,9 @@ allocate_mem_for_string(size_t upper_lim, Token *token)
  * to `argv` of `COMMAND`
  */
 int
-add_arg(char *string, size_t upper_lim, Token *token)
+add_arg (char *string, size_t upper_lim, Token *token)
 {
-  int return_val = allocate_mem_for_string(upper_lim, token);
+  int return_val = allocate_mem_for_string (upper_lim, token);
 
   if (return_val == -1)
     {
@@ -128,50 +89,97 @@ add_arg(char *string, size_t upper_lim, Token *token)
   size_t argc = token->argc;
 
   /* Copy the contents from `string` to argv array, including '\0' */
-  snprintf(argv[argc - 1], upper_lim + 1, "%s", string);
+  snprintf (argv[argc - 1], upper_lim + 1, "%s", string);
 
   return 0;
 }
 
 int
-tokenize_command(char *string, size_t *advance, Token *token)
+tokenize_command (char *string, size_t *advance, Token *token)
 {
   size_t arg_len = 0;
   *advance = 1;
 
   while (1)
-  {
-    if(string[arg_len] == ' ' || string[arg_len] == '&'
-      || string[arg_len] == '|' || string[arg_len] == ';'
-      || string[arg_len] == '(' || string[arg_len] == ')'
-      || string[arg_len] == '\0')
     {
-      reallocate_arg_array(token);
-      add_arg(string, arg_len, token);
+      /* 
+       * If any of the defined `Token_type` is found, create an 
+       * for the `COMMAND` token
+       */
+      if (string[arg_len] == ' ' || string[arg_len] == '&'
+          || string[arg_len] == '|' || string[arg_len] == ';'
+          || string[arg_len] == '(' || string[arg_len] == ')'
+          || string[arg_len] == '\0')
+        {
+          reallocate_arg_array (token);
+          add_arg (string, arg_len, token);
 
-      *advance += arg_len;
-      
-      if (string[arg_len] != ' ')
-      {
-        string += *advance;
-        break;
-      }
-      
-      string += *advance;
-      arg_len = 0;
-      continue;
+          /* Move advance till the next argument (if any) */
+          *advance += arg_len;
+
+          if (string[arg_len] != ' ')
+            {
+              string += *advance;
+              break;
+            }
+
+          string += *advance;
+          arg_len = 0;
+          continue;
+        }
+
+      arg_len += 1;
     }
 
-    arg_len += 1;
-  }
+  reallocate_arg_array (token);
 
-  reallocate_arg_array(token);
+  return 0;
+}
+
+/*
+ * Initializes the token with default values
+ */
+Token
+init_token (Token_type token_type)
+{
+  Token token;
+
+  token.type = token_type;
+  token.argv = NULL;
+  token.argc = 0;
+  token.left = NULL;
+  token.right = NULL;
+  token.return_status = NOT_DEFINED;
+
+  return token;
+}
+
+/*
+ * Adds token in `tokens` array
+ */
+int
+add_token (Token **tokens, ssize_t *token_index, Token_type token_type)
+{
+  /* `*token_index` starts from `-1` */
+  *token_index = *token_index + 1;
+
+  *tokens = realloc (*tokens, (*token_index + 1) * sizeof (**tokens));
+
+  /* `realloc` fails */
+  if (*tokens == NULL)
+    {
+      perror ("realloc failed in `add_token`");
+      return -1;
+    }
+
+  /* Initialize the token with default values */
+  (*tokens)[*token_index] = init_token (token_type);
 
   return 0;
 }
 
 void
-find_token_type(char *string, size_t *advance, Token_type *token_type)
+find_token_type (char *string, size_t *advance, Token_type *token_type)
 {
   switch (*string)
     {
@@ -180,13 +188,12 @@ find_token_type(char *string, size_t *advance, Token_type *token_type)
       if (string[1] == '|')
         {
           *token_type = LOGIC_OR;
-          *advance    = 2;
+          *advance = 2;
         }
-
       else
         {
           *token_type = PIPE;
-          *advance    = 1;
+          *advance = 1;
         }
 
       break;
@@ -196,13 +203,13 @@ find_token_type(char *string, size_t *advance, Token_type *token_type)
       if (string[1] == '&')
         {
           *token_type = LOGIC_AND;
-          *advance    = 2;
+          *advance = 2;
         }
 
       else
         {
           *token_type = BG_OPERATOR;
-          *advance    = 1;
+          *advance = 1;
         }
 
       break;
@@ -210,44 +217,44 @@ find_token_type(char *string, size_t *advance, Token_type *token_type)
     /* `LEFT_PAREN` */
     case '(':
       *token_type = LEFT_PAREN;
-      *advance    = 1;
+      *advance = 1;
 
       break;
 
     /* `RIGHT_PAREN` */
     case ')':
       *token_type = RIGHT_PAREN;
-      *advance    = 1;
+      *advance = 1;
 
       break;
 
     /* `NEXT` */
     case ';':
       *token_type = NEXT;
-      *advance    = 1;
+      *advance = 1;
 
       break;
 
     /* No more characters in string are left */
     case '\0':
       *token_type = NIL;
-      *advance    = 0;
+      *advance = 0;
 
       break;
 
     /* `COMMANDS` */
     default:
       *token_type = COMMAND;
-      *advance    = 0;
+      *advance = 0;
 
       break;
     }
 }
 
 Token *
-tokenize(char *string)
+tokenize (char *string)
 {
-  Token *tokens       = NULL;
+  Token *tokens = NULL;
   ssize_t token_index = -1;
 
   /* Analyse the string, character by character */
@@ -260,28 +267,28 @@ tokenize(char *string)
           continue;
         }
 
-      size_t advance        = 0; /* move the string pointer by this */
+      size_t advance = 0; /* move the string pointer by this */
       Token_type token_type = NIL;
 
       /* If `*string` is '\0', `token_type` will become `NIL` */
-      find_token_type(string, &advance, &token_type);
+      find_token_type (string, &advance, &token_type);
 
       /* Add a new token in `tokens` array */
-      int return_val = add_token(&tokens, &token_index, token_type);
+      int return_val = add_token (&tokens, &token_index, token_type);
 
       /* in case of error, delete already created tokens */
       if (return_val == -1)
         {
-          clean_memory(&tokens, token_index);
+          clean_memory (&tokens, token_index);
           return NULL;
         }
 
       if (token_type == COMMAND)
-        return_val = tokenize_command(string, &advance, &tokens[token_index]);
+        return_val = tokenize_command (string, &advance, &tokens[token_index]);
 
       if (return_val == -1)
         {
-          clean_memory(&tokens, token_index);
+          clean_memory (&tokens, token_index);
           return NULL;
         }
 
